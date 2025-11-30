08f6cd4ea80ac34698d0f1d23fb11629650138d9
Davide Leo <davideleo@Mac.fritz.box>
2025-12-01 00:02:54 +0100

    feat: leaky_relu, relu6

--- src/deepi/modules/activations/core.py
+++ src/deepi/modules/activations/core.py
@@ -1,7 +1,4 @@
-from abc import abstractmethod
-
 import numpy as np
-
 from deepi.modules import Module
 
 
@@ -14,18 +11,50 @@ class Activation(Module):
         super().__init__(f"activation.{_type}", False)
 
 
+class LeakyReLU(Activation):
+
+    def __init__(self, negative_slope: float = 0.01):
+        super().__init__("leaky_relu")
+        self.negative_slope = negative_slope
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        mask = x > 0
+        y = x * mask + x * self.negative_slope * (~mask)
+        if self._is_training:
+            self.dx = mask + (~mask) * self.negative_slope
+        return y
+
+    def backward(self, dy: np.ndarray) -> np.ndarray:
+        return dy * self.dx
+
+
 class ReLU(Activation):
 
     def __init__(self):
         super().__init__("relu")
 
     def forward(self, x: np.ndarray) -> np.ndarray:
+        mask = x > 0
+        y = x * mask
         if self._is_training:
-            self.dx = x > 0.0
-            y = np.where(self.dx, x, 0.0)
-        else:
-            y = np.maximum(x, 0.0)
+            self.dx = mask
         return y
 
     def backward(self, dy: np.ndarray) -> np.ndarray:
-        return np.where(self.dx, dy, 0.0) 
+        return np.where(self.dx, dy, 0.0)
+
+
+class ReLU6(Activation):
+
+    def __init__(self):
+        super().__init__("relu6")
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        mask = (x > 0.0) & (x < 6.0)
+        y = mask * x + (~mask) * ((x > 6.0) * 6.0)
+        if self._is_training:
+            self.dx = mask
+        return y
+
+    def backward(self, dy: np.ndarray) -> np.ndarray:
+        return np.where(self.dx, dy, 0.0)
