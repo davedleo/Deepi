99dc2174a3cc54c40b49de262b59b9b4bfb26336
davedleo <davedleo@users.noreply.github.com>
2025-12-04 19:22:04 +0100

    feat: initializers setup

---
+++ src/deepi/modules/initialization/__init__.py
@@ -0,0 +1,7 @@
+from .core import (
+    Initializer
+)
+
+__all__ = [
+    "Initializer"
+]
---
+++ src/deepi/modules/initialization/core.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import numpy as np
+
+from deepi.modules import Module
+
+
+class Initializer(ABC):
+
+    def __init__(self, _type: str):
+        self._type = f"initializer.{_type}"
+
+    def __call__(self, module: Module):
+        if module.has_params:
+            params = module.get_params()
+            for k, v in params.items():
+                if (k != "b") or ("running" not in k.split(".")):
+                    params[k] = self.initialize(v)
+
+    def fans(self, shape: Tuple[int, ...]) -> Tuple[float, float]:
+        n_axis = len(shape)
+        if n_axis == 2:
+            fan_in, fan_out = shape
+        elif n_axis == 3:
+            out_channels, in_channels, kernel_size = shape
+            fan_in = in_channels * kernel_size
+            fan_out = out_channels * kernel_size
+        elif n_axis == 4:
+            out_channels, in_channels, kernel_height, kernel_width = shape
+            receptive_field_size = kernel_height * kernel_width
+            fan_in = in_channels * receptive_field_size
+            fan_out = out_channels * receptive_field_size
+        else:
+            raise NotImplementedError()
+
+        return fan_in, fan_out
+
+    @abstractmethod
+    def initialize(self, shape: Tuple[int, ...]) -> np.ndarray:
+        raise NotImplementedError()
