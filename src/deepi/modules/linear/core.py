d41d8d41650d83b21342ffbb62d9497bb1513b3d
davedleo <davedleo@users.noreply.github.com>
2025-12-04 18:51:26 +0100

    fix: grads with lambdas

--- src/deepi/modules/linear/core.py
+++ src/deepi/modules/linear/core.py
@@ -62,3 +62,48 @@ class Dense(Linear):
         wT = self.cache
         dy = dy @ wT
         return dy
+
+
+class LowRank(Linear): 
+
+    def __init__(
+            self, 
+            out_features: int,
+            rank: int = 1,
+            bias: bool = True
+    ): 
+        super().__init__("low_rank", out_features, bias)
+        self.params["w1"] = (rank,)
+        self.params["w2"] = (rank, out_features)
+
+    def set_input(self, x: np.ndarray): 
+        _, in_features = x.shape
+        rank = self.params["w1"][0]
+        self.params["w1"] = (in_features, rank)
+
+    def forward(self, x: np.ndarray) -> np.ndarray: 
+        w1 = self.params["w1"].copy()
+        w2 = self.params["w2"].copy()
+        xw1 = x @ w1 
+        y = xw1 @ w2
+
+        if self._is_training: 
+            self.cache = (w1, w2)
+            self.grads["w1"] = lambda dy: x.T.copy() @ dy @ w2.T 
+            self.grads["w2"] = lambda dy: xw1.T @ dy
+            if self._has_bias: 
+                b = self.params["b"] 
+                y += b
+                self.grads["b"] = lambda dy: dy.sum(0, keepdims=True)
+
+        elif self._has_bias: 
+            b = self.params["b"]
+            y += b
+
+        return y
+
+    def backward(self, dy: np.ndarray) -> np.ndarray: 
+        w1, w2 = self.cache 
+        dy = dy @ w2.T 
+        dy = dy @ w1.T
+        return dy
