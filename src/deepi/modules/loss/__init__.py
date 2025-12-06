3498953fda48663f7425497d24c703ac606d9ada
davedleo <davedleo@users.noreply.github.com>
2025-12-07 00:39:54 +0100

    feat: kldiv

--- src/deepi/modules/loss/__init__.py
+++ src/deepi/modules/loss/__init__.py
@@ -1,6 +1,7 @@
 from .base import Loss
 from .cross_entropy import CrossEntropy
 from .elastic_net import ElasticNet
+from .kl_div import KLDiv
 from .mae import MAE
 from .mse import MSE 
 from .rmse import RMSE
@@ -9,6 +10,7 @@ __all__ = [
     "Loss",
     "CrossEntropy",
     "ElasticNet",
+    "KLDiv",
     "MAE",
     "MSE",
     "RMSE"
