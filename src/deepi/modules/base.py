feat: model and fix gradient flow

src/deepi/modules/base.py
@@ -1,4 +1,5 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod 
+from collections import defaultdict
 from typing import Dict, List, Optional, Tuple, Union
 import numpy as np
 
@@ -19,7 +20,7 @@ class Module(ABC):
 
         self._is_training: bool = False
         self.params: Dict[str, np.ndarray] = dict()
-        self.grads: Dict[str, np.ndarray] = dict()
+        self.grads: Dict[str, np.ndarray] = defaultdict(lambda: None)
 
     @abstractmethod
     def transform(self, x: ArrayOrTuple) -> ArrayOrTuple:
@@ -68,7 +69,16 @@ class Module(ABC):
                 raise TypeError("Mismatched dy types when accumulating gradients")
 
         # Compute gradient(s) to propagate upstream
-        dx = self.gradients(self.dy)
+        gradients = self.gradients(dy_copy)
+        if self._has_params: 
+            dx, grads = gradients 
+            for k, v in grads.items():
+                if self.grads[k] is None:
+                    self.grads[k] = np.zeros_like(v)
+                self.grads[k] += v
+
+        else: 
+            dx = gradients
 
         # Propagate upstream according to dx shape
         if isinstance(dx, tuple):
@@ -103,12 +113,11 @@ class Module(ABC):
         self._is_training = False
 
     def clear(self):
-        """Clear cached values and gradients."""
         self.x = None
         self.y = None
         self.dy = None
-        if self.has_params:
-            self.grads = {}
+        if self._has_params:
+            self.grads = {k: np.zeros_like(v) for k, v in self.params.items()}
 
     @property
     def type(self) -> str:

src/deepi/modules/linear/dense.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Dict, Tuple
 from deepi.modules.linear import Linear 
 
 
@@ -25,12 +26,12 @@ class Dense(Linear):
 
         return y
 
-    def gradients(self, dy: np.ndarray) -> np.ndarray:
+    def gradients(self, dy: np.ndarray) -> Tuple[np.ndarray, Dict[str, np.ndarray]]:
         dy_dense = dy @ self.params["w"].T
 
-        self.grads["w"] = self.x.T @ dy
-
+        grads = {}
+        grads["w"] = self.x.T @ dy
         if self._has_bias: 
-            self.grads["b"] = dy.sum(axis=0, keepdims=True)
+            grads["b"] = dy.sum(axis=0, keepdims=True)
 
-        return dy_dense
+        return dy_dense, grads

src/deepi/modules/linear/low_rank.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Dict, Tuple
 from deepi.modules.linear import Linear 
 
 
@@ -28,14 +29,16 @@ class LowRank(Linear):
 
         return y
 
-    def gradients(self, dy: np.ndarray) -> np.ndarray:
-        dyw2T = dy @ self.params["w2"].T 
+    def gradients(self, dy: np.ndarray) -> Tuple[np.ndarray, Dict[str, np.ndarray]]:
+        xw1 = self.x @ self.params["w1"]
+        dyw2T = dy @ self.params["w2"].T
         dy_low_rank = dyw2T @ self.params["w1"].T
 
-        self.grads["w1"] = self.x.T @ dy @ self.params["w2"].T
-        self.grads["w2"] = (self.x @ self.params["w1"]).T @ dy
+        grads = {}
+        grads["w1"] = self.x.T @ dyw2T
+        grads["w2"] = xw1.T @ dy
 
-        if self._has_bias: 
-            self.grads["b"] = dy.sum(axis=0, keepdims=True)
+        if self._has_bias:
+            grads["b"] = dy.sum(axis=0, keepdims=True)
 
-        return dy_low_rank
+        return dy_low_rank, grads