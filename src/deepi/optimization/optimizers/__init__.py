8567f56da87583127f4dbdf8b300683b60b70e14
davedleo <davedleo@users.noreply.github.com>
2025-12-22 15:48:33 +0100

    feat: nadam code

--- src/deepi/optimization/optimizers/__init__.py
+++ src/deepi/optimization/optimizers/__init__.py
@@ -3,6 +3,7 @@ from .adadelta import Adadelta
 from .adagrad import Adagrad
 from .adam import Adam
 from .adamax import Adamax
+from .n_adam import NAdam
 from .r_prop import Rprop
 from .rms_prop import RMSprop
 from .sgd import SGD
@@ -13,6 +14,7 @@ __all__ = [
     "Adagrad",
     "Adam",
     "Adamax",
+    "NAdam",
     "Rprop",
     "RMSprop",
     "SGD"
