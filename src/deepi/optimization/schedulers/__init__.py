f3191a565f3c169ea4a94eca94fe063303a5e749
davedleo <davedleo@users.noreply.github.com>
2025-12-24 10:12:03 +0100

    fix: wrong code

--- src/deepi/optimization/schedulers/__init__.py
+++ src/deepi/optimization/schedulers/__init__.py
@@ -1,7 +1,25 @@
-from .base import Scheduler 
-from .multiplicative import Multiplicative
+# from .base import Scheduler 
+# from .constant import Constant
+# from .cosine_annealing import CosineAnnealing  
+# from .cyclic import Cyclic
+# from .exponential import Exponential 
+# from .linear import Linear 
+# from .multi_step import MultiStep 
+# from .multiplicative import Multiplicative
+# from .one_cycle import OneCycle  
+# from .polynomial import Polynomial
+# from .step import Step
 
 __all__ = [
-    "Scheduler",
-    "Multiplicative"
+    # "Scheduler",
+    # "Constant",
+    # "CosineAnnealing",
+    # "Cyclic",
+    # "Exponential",
+    # "Linear",
+    # "MultiStep",
+    # "Multiplicative"
+    # "OneCycle",
+    # "Polynomial",
+    # "Step"
 ]
